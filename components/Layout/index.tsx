d09d7e61f47479d901797406fc8f4b486e776d0b
Agustin <agustin@cooperativamental.com>
2023-09-01 13:32:54 -0300

    ux power to export

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -15,24 +15,27 @@ const Layout: FC<any> = ({ children }) => {
         <div className='h-screen'>
             <div className="sticky top-0 z-40 h-24 flex items-center justify-between gap-x-6 bg-backg  shadow-sm px-6">
                 <div className="flex gap-8 justify-center items-center">
-                    <div className='flex flex-col gap-2'>
-                    <button
-                        type="button"
-                        className="-m-2.5 p-2.5 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
-                        onClick={cleanProject}
+                    <div className="absolute flex left-[40%] gap-8">
+                    <input type="file" id="file" onChange={(e) => openIDLFile(e, setIDL)} className="hidden" />
+                    <label
+                        htmlFor="file"
+                        className="-m-2.5 p-2.5 h-16 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md border border-border hover:bg-inputs hover:shadow-md hover:shadow-green-custom hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border cursor-pointer"
                     >
-                        <PlusIcon className="h-5 w-5" aria-hidden="true" />New IDL
-                    </button>
-                    <button
-                        type="button"
-                        className="-m-2.5 p-2.5 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
-                        onClick={() => saveIDLFile(IDL)}
-                    >
-                        <ArrowDownTrayIcon className="h-5 w-5" aria-hidden="true" /> Download IDL
-                    </button>
-                    </div>
+                        <FolderOpenIcon className="h-5 w-5" aria-hidden="true" /> Import IDL
+                    </label>
                     {
-                        router.asPath === "/templates" &&
+                        router.asPath !== "/templates" ?
+                        <button
+                        type="button"
+                        className="-m-2.5 p-2.5 h-16 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md border border-border hover:bg-inputs hover:shadow-md hover:shadow-green-custom hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
+                        onClick={() => {
+                            router.push("/templates")
+                        }}
+                    >
+                        <FolderArrowDownIcon className="h-5oko w-5" aria-hidden="true" />Export project
+                    </button>
+                    :
+                        
                         <button
                             type="button"
                             className="-m-2.5 p-2.5 h-16 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md border border-border hover:bg-inputs hover:shadow-md hover:shadow-green-custom hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
@@ -43,23 +46,7 @@ const Layout: FC<any> = ({ children }) => {
                             <PencilSquareIcon className="h-5oko w-5" aria-hidden="true" />IDL Generator
                         </button>
                     }
-                    <div className="absolute flex left-[40%] gap-8">
-                    <input type="file" id="file" onChange={(e) => openIDLFile(e, setIDL)} className="hidden" />
-                    <label
-                        htmlFor="file"
-                        className="-m-2.5 p-2.5 h-16 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md border border-border hover:bg-inputs hover:shadow-md hover:shadow-green-custom hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border cursor-pointer"
-                    >
-                        <FolderOpenIcon className="h-5 w-5" aria-hidden="true" /> Import IDL
-                    </label>
-                    <button
-                        type="button"
-                        className="-m-2.5 p-2.5 h-16 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md border border-border hover:bg-inputs hover:shadow-md hover:shadow-green-custom hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
-                        onClick={() => {
-                            router.push("/templates")
-                        }}
-                    >
-                        <FolderArrowDownIcon className="h-5oko w-5" aria-hidden="true" />Export project
-                    </button>
+                   
                     </div>
                     <svg className="animate-spin stroke-white h-5 w-5 mr-3" viewBox="0 0 24 24">
                        
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NewEditor } from "@/components/ViewTables/Editor";
 import ClassicEditor from "@/components/ViewCards/Editor";
 import JSONEditor from "@/components/JSONEditor";
+import { ArrowDownTrayIcon, PlusIcon } from '@heroicons/react/24/solid';
 import { CodeBracketIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
@@ -49,13 +50,31 @@ export default function Home() {
   }
 
   return (
-    <div className='h-screen'>
-      <div className="sticky top-0 h-20 flex items-center text-center gap-x-6 px-6">
-        <div className="flex items-center text-border gap-5">
+    <div className='h-full flex flex-col gap-2'>
+      <div className="sticky flex-col top-0 h-20 flex gap-4 px-6">
+      <div className='flex gap-2'>
+                    <button
+                        type="button"
+                        className="-m-2.5 p-2.5 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
+                        // onClick={cleanProject}
+                    >
+                        <PlusIcon className="h-5 w-5" aria-hidden="true" />New IDL
+                    </button>
+                    <button
+                        type="button"
+                        className="-m-2.5 p-2.5 text-chok text-sm inline-flex items-center gap-x-1.5 rounded-md hover:text-green-custom focus:bg-inputs active:outline-none active:ring active:ring-border"
+                        // onClick={() => saveIDLFile(IDL)}
+                    >
+                        <ArrowDownTrayIcon className="h-5 w-5" aria-hidden="true" /> Download IDL
+                    </button>
+        </div>
+        <div className="flex flex-col text-border gap-1">
           <p className="text-border">select view:</p>
-          <button onClick={() => setSelectedUI("cards")} className={`hover:underline bg-inputs p-1 border-md ${selectedUI === "cards" && "underline text-chok"}`}>cards</button>
-          <button onClick={() => setSelectedUI("tables")} className={`hover:underline bg-inputs p-1 border-md ${selectedUI === "tables" && "underline text-chok"}`}>tables</button>
-          <button onClick={() => setSelectedUI("json")} className={`hover:underline bg-inputs p-1 border-md ${selectedUI === "json" && "underline text-chok"}`}>JSON</button>
+          <div className="flex">
+          <button onClick={() => setSelectedUI("cards")} className={`hover:underline p-1 border-md ${selectedUI === "cards" && "underline text-chok"}`}>cards</button>
+          <button onClick={() => setSelectedUI("tables")} className={`hover:underline p-1 border-md ${selectedUI === "tables" && "underline text-chok"}`}>tables</button>
+          <button onClick={() => setSelectedUI("json")} className={`hover:underline p-1 border-md ${selectedUI === "json" && "underline text-chok"}`}>JSON</button>
+          </div>
         </div>
       </div>
       <div className=" h-[calc(100%_-_5rem)] bg-backg ">
