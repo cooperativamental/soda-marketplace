c0430c79720aa0fe312f59043a3f52347e2c0424
Agustin <agustin@cooperativamental.com>
2023-09-04 19:53:00 -0300

    va quedando

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -13,9 +13,12 @@ const Templates = () => {
 
     return (
         <div className="flex flex-col h-full ">
-            <h1 className="text-2xl text-white font-bold ml-5">{IDL.name || "Edit IDL or Import IDL"}</h1>
+            <div className="h-20">
+            <h4 className="text-md text-chok text-center font-light ml-5">Export project:</h4>
+            <h1 className="text-2xl text-chok text-center font-bold ml-5">{IDL.name || "Import or create IDL"}</h1>
+                
+            </div>
             <div className=" flex gap-4 px-4 w-full h-[calc(100%-6rem)] rounded-3xl border-border">
-
                 <div className="flex flex-col h-96 w-72 !border !border-border !p-5 !shadow-md !shadow-black  !text-chok !text-center !gap-3 !items-center !cursor-pointer !rounded-3xl !font-thin">
                     {
                         (!connection || !wallet) ?
@@ -31,12 +34,12 @@ const Templates = () => {
                                 }}
                             />
                             :
-                            "QR"
+                            "Get a soda for your template. Use this QR to mint your sodas"
                     }
                 </div>
                 <div className="flex flex-col w-full justify-between">
                     <div
-                        className={`grid grid-cols-4 gap-4 ${!templates.length ? "!flex items-center justify-center w-full" : "" } w-full h-[48%] rounded-t-2xl overflow-y-auto border-2 p-4 mini-scrollbar`}
+                        className={`grid grid-cols-4 gap-4 ${!templates.length ? "!flex items-center justify-center w-full" : "" } w-full h-[48%] overflow-y-auto p-4 mini-scrollbar`}
                     >
                         {
                             templates.length ?
@@ -53,7 +56,7 @@ const Templates = () => {
                         }
                     </div>
                     <div
-                        className={`grid grid-cols-4 gap-4 ${!templates.length ? "!flex items-center justify-center w-full" : "" } w-full h-[48%] rounded-b-2xl overflow-y-auto border-2 p-4 mini-scrollbar`}
+                        className={`grid grid-cols-4 gap-4 ${!templates.length ? "!flex items-center justify-center w-full" : "" } w-full h-[48%] overflow-y-auto p-4 mini-scrollbar`}
                         >
                         {
                             templates.length ?
@@ -72,7 +75,7 @@ const Templates = () => {
                 </div>
             </div>
             <div className="self-center text-xs text-white p-5">
-                Dev a Template
+            🌌🔧 Build templates like a pro with Soda. Craft templates that simplify the blockchain experience. Ping here 🚀🧩
             </div>
         </div>
     )
