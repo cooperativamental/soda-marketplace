c8697aec4e04b2b62fdb63a3a6ae215dc5dc48b3
SergioMentalCoop <sergio@cooperativamental.com>
2023-09-05 14:56:02 -0300

    bubbles and loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
 
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -10,12 +11,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    values: {
+
+    },
     extend: {
       boxShadow: {
         'tab': '0px 1px 0px -0.5px',
         'tabSelected': "0px 0px 4px -2px "
       },
-      colors:  {
+      colors: {
         'backg': '#081635',
         'inputs': '#102042',
         'blue-custom': '#000031',
@@ -27,10 +31,24 @@ module.exports = {
         'border': '#334155',
         'export': '#387847',
       },
+      backgroundImage: {
+        'bubble': "url('../public/bluebubble.svg')"
+      },
+      keyframes: {
+        bubble: {
+          '0%': { backgroundPosition: "0 1000px" },
+          '100%': { backgroundPosition: '0 0' },
+        }
+      },
+      animationDelay: {
+        100: "100ms",
+        200: "200ms",
+        300: "300ms"
+      }
     },
   },
   plugins: [
-    // ...
     require('@tailwindcss/forms'),
+    require('./pluginsTailwind/animationDelay')
   ],
 }
