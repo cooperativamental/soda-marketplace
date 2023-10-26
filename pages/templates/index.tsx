04fc76b5c2b9a1da0372e038fa155efb0190dbad
Agustin <agustin@cooperativamental.com>
2023-10-26 19:34:48 -0300

    last track

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -7,6 +7,7 @@ import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapte
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from "react"
 import Link from 'next/link'
+import { track } from "@vercel/analytics"
 
 type NFTAccess = {
     address: string,
@@ -74,31 +75,31 @@ const Templates = () => {
 
                 </div>
                 <div className="md:col-start-1 md:col-end-2 row-start-3 md:row-start-2 md:row-end-3 flex flex-col h-76 w-full md:w-72 !border !border-border !p-5 !shadow-md !shadow-black  !text-chok !text-left !gap-3  self-center !rounded-3xl !font-thin">
-                    <div>
+                    <div onClick={() => track("deskApp")}>
                         <Link href="https://github.com/Web3-Builders-Alliance/soda/tags" target="_blank"><h5 className="font-bold hover:text-green-custom">
                             🖥️  Download Soda Desktop app </h5></Link>
                         <p>Free for students and community courses</p>
                     </div>
-                    <div>
+                    <div onClick={() => track("contribute")}>
                         <h5 className="font-bold">🥤 Soda is open-source.</h5>
                         <Link href="https://github.com/Web3-Builders-Alliance/soda/" target="_blank"> <p className="hover:text-green-custom">Contribute here.</p></Link>
                     </div>
-                    <div>
+                    <div onClick={() => track("feedback")}>
                         <Link href="https://x.com/use_soda" target="_blank"><h5 className="font-bold hover:text-green-custom">🚀 Send us your feedback</h5></Link>
                         <p>Your experience using Soda is a valuable asset for our dev team.</p>
                     </div>
                 </div>
                 <div className="md:col-start-1 md:col-end-2 row-start-4 md:row-start-3 md:row-end-4 flex flex-col h-76 w-full md:w-72 !border !border-border !p-5 !shadow-md !shadow-black  !text-chok !text-left !gap-3 self-center justify-center  !rounded-3xl !font-thin">
-                    <div>
+                    <div onClick={() => track("exchange")}>
                         <Link href="https://solana.stackexchange.com/" target="_blank"><h5 className="font-bold hover:text-green-custom">🤔 Questions?</h5>
                             <p>solana.stackexchange.com</p>
                         </Link>
                     </div>
-                    <div>
+                    <div onClick={() => track("cookbook")}>
                         <Link href="https://solanacookbook.com/" target="_blank"><h5 className="font-bold hover:text-green-custom">📖 Solana Cookbook</h5></Link>
                         <p>Read the Solana Cookbook</p>
                     </div>
-                    <div>
+                    <div onClick={() => track("solanaTools")}>
                         <Link href="https://github.com/solana-developers/solana-tools" target="_blank"><h5 className="font-bold hover:text-green-custom">🦀  Solana Tools 🪓</h5></Link>
                         <p>List of all Solana dev tools </p>
                     </div>
